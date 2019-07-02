e826efdb8a66ae0e2b9b0e5b9c5133e672d780f8
Дмитрий <rabolan@mail.ru>
2019-07-02 21:58:48 +0300

    Рабочая лока база

---
+++ GameStore.Domain/Concrete/EFGameRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameStore.Domain.Entities;
+using GameStore.Domain.Abstract;
+
+namespace GameStore.Domain.Concrete
+{
+    /// <summary>
+    /// Класс для контекста подключения к БДы
+    /// Служит для извлечение даных изБД
+    /// </summary>
+    public class EFGameRepository : IGameRepository
+    {
+        EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Game> Games
+        {
+            get { return context.Games; }
+        }
+    }
+}
--- GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -6,6 +6,8 @@ using Moq;
 using Ninject;
 using GameStore.Domain.Abstract;
 using GameStore.Domain.Entities;
+using GameStore.Domain.Concrete;
+
 
 namespace GameStore.WebUI.Infrastructure
 {
@@ -38,15 +40,15 @@ namespace GameStore.WebUI.Infrastructure
         private void AddBindings()
         {
             // Здесь размещаются привязки
-
-            Mock<IGameRepository> mock = new Mock<IGameRepository>();
-            mock.Setup(m => m.Games).Returns(new List<Game>
-    {
-        new Game { Name = "SimCity", Price = 1499 },
-        new Game { Name = "TITANFALL", Price=2299 },
-        new Game { Name = "Battlefield 4", Price=899.4M }
-    });
-            kernel.Bind<IGameRepository>().ToConstant(mock.Object);
+            kernel.Bind<IGameRepository>().To<EFGameRepository>();
+            //        Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            //        mock.Setup(m => m.Games).Returns(new List<Game>
+            //{
+            //    new Game { Name = "SimCity", Price = 1499 },
+            //    new Game { Name = "TITANFALL", Price=2299 },
+            //    new Game { Name = "Battlefield 4", Price=899.4M }
+            //});
+            //        kernel.Bind<IGameRepository>().ToConstant(mock.Object);
         }
 
         /// <summary>
