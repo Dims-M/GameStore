a91e18650f6f007bfaf167080d2cfa40ddb9c235
Дмитрий <rabolan@mail.ru>
2019-07-02 21:33:49 +0300

    добавление бд

---
+++ GameStore.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameStore.Domain.Entities;
+using System.Data.Entity;
+
+namespace GameStore.Domain.Concrete
+{
+    //Класс привязка к бд ентинити
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+    }
+}
