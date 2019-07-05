2ca7a3034ebe24bc5c00403d066658bdb262259e
Дмитрий <rabolan@mail.ru>
2019-07-06 00:40:01 +0300

    cnhfybwb

---
+++ GameStore.WebUI/Models/PagingInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameStore.WebUI.Models
+{
+    /// <summary>
+    /// класс для передачи данных между контроллером и представлением
+    /// </summary>
+    public class PagingInfo
+    {
+        // Кол-во товаров
+        public int TotalItems { get; set; }
+
+        // Кол-во товаров на одной странице
+        public int ItemsPerPage { get; set; }
+
+        // Номер текущей страницы
+        public int CurrentPage { get; set; }
+
+        // Общее кол-во страниц
+        public int TotalPages
+        {
+            //округляет полученное значение страниц
+            get { return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage); }
+        }
+    }
+}
